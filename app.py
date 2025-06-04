365711fdbb58cb4672d4719da02094c3d0cb18fd
Prashu-001 <prashu150603@gmail.com>
2025-06-04 20:31:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import time
-import tensorflow
+import numpy as np
 from utils.model_loader import load_wgan, load_srgan, load_srwgan
 from utils.image_utils import preprocess_image, postprocess_image
 
@@ -149,9 +149,9 @@ elif page == "Restore Images":
                     start_time = time.time()
                     output = model(input_tensor,training=False)
                     elapsed_time = time.time() - start_time
-
-                    output_img = tensorflow.clip_by_value((output[0] + 1.0) * 127.5, 0, 255)
-                    output_img = tensorflow.cast(output_img, tensorflow.uint8).numpy()
+                    
+                    output=output.numpy()
+                    output_img=((output[0]+1.0)*127.5).clip(0,255).astype(np.uint8)
                     original_img = np.array(img.resize(output_img.shape[:2][::-1]))
 
                     psnr_val, ssim_val, lips_val = compute_metrics(original_img, output_img)
