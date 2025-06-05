3bae12dee38ea294f618a6acabf3f58263efdb08
bidyut-25 <bidyutsupakar2000@gmail.com>
2025-06-05 10:59:46 +0530

    Update model_loader.py

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,5 +1,19 @@
 import tensorflow as tf
 from SRgan import Generator
+
+class DepthToSpace(tf.keras.layers.Layer):
+    def __init__(self, block_size, **kwargs):
+        super().__init__(**kwargs)
+        self.block_size = block_size
+
+    def call(self, x):
+        return tf.nn.depth_to_space(x, self.block_size)
+
+    def get_config(self):
+        config = super().get_config()
+        config['block_size'] = self.block_size
+        return config
+
 def load_wgan():
     # Your code here to load WGAN TF model
     model = tf.keras.models.load_model('models/wgan_model.h5')
@@ -11,5 +25,5 @@ def load_srgan():
     return mode1
 
 def load_srwgan():
-    model = tf.keras.models.load_model('models/srwgan_model.h5')
+    model = tf.keras.models.load_model('models/srwgan_model.h5',custom_objects={'DepthToSpace':DepthToSpace})
     return model
