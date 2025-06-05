d0d86a6abde0071dc0ad6e322ef0321c78b532ac
bidyut-25 <bidyutsupakar2000@gmail.com>
2025-06-05 22:25:59 +0530

    Update model_loader.py

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -14,11 +14,6 @@ class DepthToSpace(tf.keras.layers.Layer):
         config['block_size'] = self.block_size
         return config
 
-def load_wgan():
-    # Your code here to load WGAN TF model
-    model = tf.keras.models.load_model('models/wgan_model.h5')
-    return model
-
 def load_srgan():
     mode1 = Generator((64,64,3),10)
     mode1.load_weights('models/srgan_model.h5')
